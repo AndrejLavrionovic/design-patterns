e910457d7b3814dcd1dcf20b02fe2ac362a6bd41
Andrej Lavrinovic <andrei.lavrinovic@gmail.com>
2018-09-08 19:57:41 +0100

    Knight is added.

--- modules/adventure-game/src/main/java/ie/altech/game/adventure/Runner.java
+++ modules/adventure-game/src/main/java/ie/altech/game/adventure/Runner.java
@@ -1,8 +1,28 @@
 package ie.altech.game.adventure;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import ie.altech.game.adventure.characters.Character;
+import ie.altech.game.adventure.characters.Knight;
+
 public class Runner{
 
     public static void main (String ... args) {
-        System.out.println("Adventure Game is running OK!");
+
+        List<Character> characters = new ArrayList<>();
+        characters.add(new Knight());
+
+        System.out.println("\n--- Java application is working OK!");
+
+        characters.stream().forEach(character -> characterDescription(character));
+    }
+
+    private static void characterDescription(Character character){
+        System.out.print("\n--- ");
+        character.character();
+        System.out.println("      Fight with:");
+        character.fight();
+        System.out.println();
     }
 }
